968fa581b1fe1f3b4d55739d6fe36156e8a48610
MwangiN <65793184+MwangiN@users.noreply.github.com>
2021-04-14 19:07:07 +0300

    new modules 3 ad 4

---
+++ pluralsight/condlogic.java
@@ -0,0 +1,20 @@
+package pluralsight;
+
+public class condlogic {
+    public static void main(String[] args) {
+    // if else
+    int value1 = 1;
+    int value2 = 7;
+    /*
+    if (value1>value2)
+        System.out.println("value 1 is bigger");
+    else
+        System.out.println("value 1 is not bigger");
+*/
+        // if else if 
+        if (value1 > value2)
+        System.out.println("value 1 is bigger");
+    else if(value1 < value2)
+        System.out.println("value 2 is bigger");
+}
+    }
